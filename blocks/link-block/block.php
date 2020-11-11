3771e1845b425a2c4f87eca90744c9d1cc83f37b
Rhylen 'Jennifer' Nguyen <jtn1@email.sc.edu>
2020-11-10 23:34:31 -0500

    update

--- blocks/link-block/block.php
+++ blocks/link-block/block.php
@@ -1,13 +1,13 @@
 <style>
-  .link-block {
+  #<?php block_field('id');?>{
       box-shadow: 0px 5px <?php block_field('color');?>;
   }
 </style>
 
 <a class="listlink" href="<?php block_field( 'link' ); ?>">
-<div class="link-block">
+<div class="link-block" id="<?php block_field('id');?>">
   <div class="svg">
-    <?php block_field( 'logo' ); ?>
+    <?php block_field( 'svg' ); ?>
     <p> <?php block_field('task');?> </p>
     <p id="desc"><?php block_field('desc')?></p>
   </div>
