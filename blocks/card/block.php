b6bb7be7096379f302ea3f8254ca5e512663b1c1
Rhylen 'Jennifer' Nguyen <jtn1@email.sc.edu>
2020-11-11 18:20:47 -0500

    update

--- blocks/card/block.php
+++ blocks/card/block.php
@@ -1,5 +1,5 @@
 <a href="<?php block_field('link');?>">
-  <div class="card" id="<?php block_field('id')?>">
+  <div class="card <?php block_field('color');?>" id="<?php block_field('id')?>">
     <p><?php block_field('text')?></p>
   </div>
 </a>
--- blocks/yellow-card/block.php
+++
@@ -1,5 +0,0 @@
-<a href="<?php block_field('link');?>">
-  <div class="card" id="<?php block_field('id')?>">
-    <p><?php block_field('text')?></p>
-  </div>
-</a>
